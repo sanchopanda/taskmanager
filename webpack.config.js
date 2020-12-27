fcc09e7589714a657df7f615e725ef3d14336015
Ivanov Alexander <a.ivanov@uplab.ru>
2020-12-27 22:33:34 +0300

    setup build

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require(`path`);
+module.exports = {
+    mode: `development`,
+// Режим сборки
+    entry: `./src/main.js`,
+// Точка входа приложения
+    output: {
+// Настройка выходного файла
+        filename: `bundle.js`,
+        path: path.join(__dirname, `public`)
+    },
+    devtool: `source-map`
+};
